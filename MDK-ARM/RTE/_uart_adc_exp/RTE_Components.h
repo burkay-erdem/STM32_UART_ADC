42fc4de8885722869b272ae9ed78c987fab93b4f
erdem3801 <burkaerdem@gmail.com>
2019-11-04 22:06:49 +0300

    hojam kolay gele

---
+++ MDK-ARM/RTE/_uart_adc_exp/RTE_Components.h
@@ -0,0 +1,20 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'uart_adc_exp' 
+ * Target:  'uart_adc_exp' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f4xx.h"
+
+
+#endif /* RTE_COMPONENTS_H */
